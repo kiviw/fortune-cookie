4b1519f639b779c784cb6b6f63be7c9fea7ee5f4
kiviw <100179542+kiviw@users.noreply.github.com>
2023-09-10 14:28:06 +0300

    Create tronweb.js

---
+++ js/tronweb.js
@@ -0,0 +1,9 @@
+// This is a simplified example; you should use the actual TRONWeb library
+// Replace with your actual TRONWeb integration code
+const tronWeb = {
+    sendTransaction: async (to, amount) => {
+        // Simulate a TRX transaction
+        console.log(`Sending ${amount} TRX to ${to}`);
+        return true; // Simulated success
+    }
+};
